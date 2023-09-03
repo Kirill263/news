d104b623e4c402b3d2619cff0c5e05af2cb72d18
Кирилл <myakiishevkirill@gmail.com>
2023-09-03 17:10:58 +1000

    added pagination for news and categories on the main page and for the test page

--- news/views.py
+++ news/views.py
@@ -4,13 +4,24 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
 from .models import News, Category
 from .forms import NewsForm
+from django.core.paginator import Paginator
+
+
+def test(request):
+    objects = ['join1', 'paul2', 'george3', 'ringo4', 'join5', 'paul6', 'george7']
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator.get_page(page_num)
+    return render(request, 'news/test.html', {'page_obj': page_objects})
+    
 
 
 class HomeNews(ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
-    # extra_context = {'title': 'Главная'}
+    paginate_by = 2
+    
     
     
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -28,6 +39,7 @@ class NewsByCategory(ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 2
     
     
     def get_context_data(self, *, object_list=None, **kwargs):
